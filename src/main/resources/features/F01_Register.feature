daea0d382407dcaa13e54784669820737c003f2a
Mark Essam Hakim <63367712+markessam12@users.noreply.github.com>
2022-09-10 14:34:25 +0200

    Test Automation Project Preparation

---
+++ src/main/resources/features/F01_Register.feature
@@ -0,0 +1,5 @@
+@smoke  @regression
+Feature: F01_Register | users could register with new accounts
+
+  Scenario: guest user could register with valid data successfully
+    Given user go to register page
---
+++ src/test/java/org/example/stepDefs/RegistrationPageTester.java
@@ -0,0 +1,11 @@
+package org.example.stepDefs;
+
+import io.cucumber.java.en.Given;
+
+public class RegistrationPageTester {
+    @Given("user go to register page")
+    public void goRegisterPage()
+    {
+        System.out.println("This is a test before start coding");
+    }
+}
